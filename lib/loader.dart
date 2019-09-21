e3d05640f0c10dda44bb9764bff86cd1ee6b30c1
Ramkishorevit <ramkishorevit@gmail.com>
2019-09-22 00:17:27 +0530

    Create loader

---
+++ lib/loader.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Loader());
+
+class Loader extends StatefulWidget with TickerProviderStateMixin {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+  @override
+  // TODO: implement context
+  BuildContext get context => null;
+
+  @override
+  void deactivate() {
+    // TODO: implement deactivate
+  }
+
+  @override
+  void didUpdateWidget(StatefulWidget oldWidget) {
+    // TODO: implement didUpdateWidget
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+  }
+
+  @override
+  // TODO: implement mounted
+  bool get mounted => null;
+
+  @override
+  void reassemble() {
+    // TODO: implement reassemble
+  }
+
+  @override
+  void setState(fn) {
+    // TODO: implement setState
+  }
+
+  @override
+  // TODO: implement widget
+  StatefulWidget get widget => null;
+
+}
